1629142ff3d9aff7560fdc59b885762dd8415bee
Wes <wesmarberry@gmail.com>
2019-04-30 10:51:24 -0500

    fixed delete user bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -395,6 +395,7 @@ router.delete('/:id', async (req, res, next) => {
 	          $in: foundUser.pets 
 	        }
 		})
+		req.session.destroy()
 		res.redirect('/users')
 	} catch (err) {
 		next(err)
